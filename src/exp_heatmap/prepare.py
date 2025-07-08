212f4f5e2beef4a4318ab08ffa52804d29ccc1e2
AdamNogell <adam.nogell@gmail.com>
2025-07-08 18:38:55 +0200

    zarr version checking added

--- src/exp_heatmap/prepare.py
+++ src/exp_heatmap/prepare.py
@@ -1,10 +1,18 @@
 import sys
 import allel
+import zarr
 
 import exp_heatmap.utils as utils
 
 
 def prepare(recode_file: str, zarr_dir: str):
+    zarr_version = zarr.__version__
+    if zarr_version.startswith('3.') or int(zarr_version.split('.')[0]) >= 3:
+        print(f"Error: zarr version {zarr_version} is not supported.")
+        print("Please downgrade to zarr version < 3.0.0:")
+        print("  pip install 'zarr<3.0.0'")
+        sys.exit(1)
+    
     utils.check_path_or_exit(recode_file)
 
     try:
