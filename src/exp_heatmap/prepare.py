688428be8da95769f1555333843912d956386d71
AdamNogell <adam.nogell@gmail.com>
2025-07-08 18:36:45 +0200

    added try-except block

--- src/exp_heatmap/prepare.py
+++ src/exp_heatmap/prepare.py
@@ -7,8 +7,12 @@ import exp_heatmap.utils as utils
 def prepare(recode_file: str, zarr_dir: str):
     utils.check_path_or_exit(recode_file)
 
-    allel.vcf_to_zarr(recode_file, zarr_dir, fields="*", log=sys.stdout)
-
+    try:
+        allel.vcf_to_zarr(recode_file, zarr_dir, fields="*", log=sys.stdout)
+    except Exception as e:
+        print(f"Error converting VCF to ZARR: {e}")
+        sys.exit(1)
+    
     print()
     print(f"Recoded VCF: {recode_file}")
     print(f"ZARR dir: {zarr_dir}")
