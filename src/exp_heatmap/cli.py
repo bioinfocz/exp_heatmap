1d747efd154be4b7da7fc5bb0df04068d46116be
AdamNogell <adam.nogell@gmail.com>
2025-07-30 19:08:07 +0200

    cli reworked using click

--- src/exp_heatmap/cli.py
+++ src/exp_heatmap/cli.py
@@ -1,112 +1,76 @@
-import argparse
 import sys
-
+import click
 from exp_heatmap import prepare, compute, plot, __version__
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="exp_heatmap",
-        usage="%(prog)s <command> [options]",
-        epilog="For more information, see the documentation at https://github.com/bioinfocz/exp_heatmap/"
-    )
-    parser.add_argument(
-        '-v', '--version', action='version', version=f'%(prog)s {__version__}'
-    )
-    subparser = parser.add_subparsers(title='Available commands', metavar=' ')
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-    parser.set_defaults(func=lambda _: parser.print_help())
+@click.group(help="ExP Heatmap - Population genetics visualization tool", epilog="For more information, see the documentation at:\nhttps://github.com/bioinfocz/exp_heatmap/", context_settings=CONTEXT_SETTINGS)
+@click.version_option(__version__, '-v', '--version', prog_name='exp_heatmap')
+def cli():
+    pass
 
+# prepare command
+@cli.command(name='prepare', context_settings=CONTEXT_SETTINGS)
+@click.argument('vcf_file', type=click.Path(exists=True, readable=True, dir_okay=False), required=True, metavar='<vcf_file>')
+@click.option('-o', '--output', type=click.Path(), default='zarr_output', help='Directory for ZARR files')
+def prepare_cmd(vcf_file, output):
+    """
+    Convert VCF file to ZARR format.
     
-    # prepare
-    prepare_parser = subparser.add_parser("prepare", help="Convert VCF file to ZARR format")
-    prepare_parser.add_argument(
-        "recode_file", metavar="RECODE_FILE", help="Path to recoded VCF file"
-    )
-    prepare_parser.add_argument(
-        "zarr_dir", metavar="ZARR_DIR", help="Path where ZARR directory will be saved"
-    )
-    prepare_parser.set_defaults(
-        func=lambda args: prepare(args.recode_file, args.zarr_dir)
-    )
+    <vcf_file>  PATH  Recoded VCF file
+    """
+    prepare(vcf_file, output)
 
+# compute command
+@cli.command(name='compute',context_settings=CONTEXT_SETTINGS)
+@click.argument('zarr_dir', type=click.Path(exists=True, readable=True, file_okay=False), required=True, metavar='<zarr_dir>')
+@click.argument('panel_file', type=click.Path(exists=True, readable=True, dir_okay=False), required=True, metavar='<panel_file>')
+@click.option('-o', '--output', type=click.Path(), default='output', help='Directory for output files')
+@click.option('-t', '--test', type=click.Choice(['xpehh', 'xpnsl', 'delta_tajima_d', 'hudson_fst'], case_sensitive=False), default='xpehh', show_default=True, help='Statistical test to compute')
+@click.option('-c', '--chunked', is_flag=True, help='Use chunked array to avoid memory exhaustion')
+def compute_cmd(zarr_dir, panel_file, output, test, chunked):
+    """
+    Compute population genetics statistics.
+    
+    \b
+    <zarr_dir>  PATH  Directory with ZARR files from 'exp_heatmap prepare'
+    <panel_file>  PATH  Population panel file
+    """
+    compute(zarr_dir, panel_file, output, test, chunked)
 
-    # compute
-    compute_parser = subparser.add_parser("compute", help="Compute population genetics statistics")
-    compute_parser.add_argument(
-        "zarr_dir", metavar="ZARR_DIR", help="Path to ZARR directory"
-    )
-    compute_parser.add_argument(
-        "panel_file", metavar="PANEL_FILE", help="Path to panel file"
-    )
-    compute_parser.add_argument(
-        "output_dir", metavar="OUTPUT_DIR", help="Directory for output files"
-    )
-    compute_parser.add_argument(
-        "-t", "--test", choices=['xpehh', 'xpnsl', 'delta_tajima_d', 'hudson_fst'], 
-        default="xpehh", help="Statistical test to compute (default: %(default)s)"
-    )
-    compute_parser.add_argument(
-        "-c", "--chunked", action="store_true", help="Use chunked array to avoid memory exhaustion"
-    )
-    compute_parser.set_defaults(
-        func=lambda args: compute(args.zarr_dir, args.panel_file, args.output_dir, args.test, args.chunked)
-    )
-
-
-    # plot
-    plot_parser = subparser.add_parser("plot", help="Generate heatmap visualization")
-    plot_parser.add_argument(
-        "input_dir", metavar="INPUT_DIR", help="Directory containing TSV files from 'exp_heatmap compute'"
-    )
-    plot_parser.add_argument(
-        "-s", "--start", type=int, default=None, help=f"Start position for the displayed region. Uses nearest available position if exact match not found in the input data [USE WITH -e]"
-    )
-    plot_parser.add_argument(
-        "-e", "--end", type=int, default=None, help="End position for the displayed region. Uses nearest available position if exact match not found in the input data [USE WITH -s]"
-    )
-    plot_parser.add_argument(
-        "-m", "--mid", type=int, default=None, help="Middle of the displayed area. The start and end positions will be calculated (mid ± 500 kb)"
-    )
-    plot_parser.add_argument(
-        "-t", "--title", default=None, help="Title of the heatmap"
-    )
-    plot_parser.add_argument(
-        "-o", "--output", default="ExP_heatmap", help="The heatmap will be saved as OUTPUT + .png (default: %(default)s)"
-    )
-    plot_parser.add_argument(
-        "--cmap", default="Blues", help="Matplotlib colormap for heatmap visualization (default: %(default)s)"
-    )
-
-    def plot_wrapper(args):
-        """
-        Validate the correct usage of position arguments and call plot function with calculated start/end coordinates.
-        """
-        has_start_end = args.start is not None and args.end is not None
-        has_mid = args.mid is not None
-        
-        if not has_start_end and not has_mid:
-            plot_parser.error("Either (--start and --end) or --mid must be provided")
-        elif has_start_end and has_mid:
-            plot_parser.error("Cannot use both (--start and --end) and --mid at the same time")
-        elif (args.start is None) != (args.end is None):
-            plot_parser.error("--start and --end must be used together")
-        
-        if has_mid:
-            start, end = args.mid - 500000, args.mid + 500000
-        else:
-            start, end = args.start, args.end
-            
-        plot(args.input_dir, start=start, end=end, title=args.title, output=args.output, cmap=args.cmap)
-
-    plot_parser.set_defaults(func=plot_wrapper)
-
-    args = parser.parse_args()
-    args.func(args)
-
+# plot command
+@cli.command(name='plot', context_settings=CONTEXT_SETTINGS)
+@click.argument('input_dir', type=click.Path(exists=True, file_okay=False), required=True, metavar='<input_dir>')
+@click.option('-s', '--start', type=int, help='Start position for the displayed region. Uses nearest available position if exact match not found in the input data [USE WITH -e]')
+@click.option('-e', '--end', type=int, help='End position for the displayed region. Uses nearest available position if exact match not found in the input data [USE WITH -s]')
+@click.option('-m', '--mid', type=int, help='Middle of the displayed area. The start and end positions will be calculated (mid ± 500 kb)')
+@click.option('-t', '--title', type=str, help='Title of the heatmap')
+@click.option('-o', '--output', type=click.Path(), default='ExP_heatmap',show_default=True, help='The output heatmap')
+@click.option('-c', '--cmap', type=str, default='Blues', show_default=True, help='Matplotlib colormap for heatmap visualization')
+def plot_cmd(input_dir, start, end, mid, title, output, cmap):
+    """
+    Generate heatmap visualization.
+    
+    <input_dir>  PATH  Directory with TSV files from 'exp_heatmap compute'
+    """
+    # TODO: Reduce the size of the validation step
+    has_start_end = start is not None and end is not None
+    has_mid = mid is not None
+    
+    if not has_start_end and not has_mid:
+        raise click.UsageError("Either (--start and --end) or --mid must be provided")
+    elif has_start_end and has_mid:
+        raise click.UsageError("Cannot use both (--start and --end) and --mid at the same time")
+    elif (start is None) != (end is None):
+        raise click.UsageError("--start and --end must be used together")
+    
+    # Calculate start and end positions
+    if has_mid:
+        start_pos, end_pos = mid - 500000, mid + 500000
+    else:
+        start_pos, end_pos = start, end
+    
+    plot(input_dir, start=start_pos, end=end_pos, title=title, output=output, cmap=cmap)
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("")
-        sys.exit(1)
+    cli()
