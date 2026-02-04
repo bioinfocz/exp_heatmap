2ecf29aecd973005f6708fe6a291e5af9fbada64
AdamNogell <adam.nogell@gmail.com>
2026-02-04 18:13:35 +0100

    Remove benchmarks

--- src/exp_heatmap/__init__.py
+++ src/exp_heatmap/__init__.py
@@ -7,7 +7,6 @@ from . import utils
 from . import rank_tools
 from . import logging as exp_logging
 from .logging import setup_logging, get_logger
-from . import benchmark
 from . import interactive
 
 __version__ = "1.2.0"
